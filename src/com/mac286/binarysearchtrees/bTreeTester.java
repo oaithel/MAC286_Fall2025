4088184effc8c191b4118cfd272b8eda792cc725
prof Omar <oaithel@yahoo.com>
2025-10-21 11:20:09 -0400

    Prof.Omar 10/21/2025

---
+++ src/com/mac286/binarysearchtrees/bTreeTester.java
@@ -0,0 +1,24 @@
+package com.mac286.binarysearchtrees;
+
+public class bTreeTester {
+    public static void main(String[] args) {
+        bTree<Integer, String> T = new bTree<>();
+        T.insert(25, "Hi25");
+        T.insert(30, "Hifgh30");
+        T.insert(10, "Hidgvj");
+        T.insert(7, "Hidgvnrt");
+        T.insert(15, "Hidgvn");
+        T.insert(13, "Hi cnbm");
+        T.insert(17, "Hibn bnm");
+        T.insert(26, "Hicbm");
+        T.insert(32, "Hvmbhmni");
+        T.insert(35, "Hivm ");
+        T.insert(18, "Hinb m");
+        T.insert(4, "Hifgbw");
+        T.insert(36, "Hsdri");
+        T.preOrdertraversal();
+
+
+
+    }
+}
