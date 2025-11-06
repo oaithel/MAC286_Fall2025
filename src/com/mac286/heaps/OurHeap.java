daab3698b1d6ab9f4c7791ddfc2beea8b04160f4
ProfOmar <oaithel@yahoo.com>
2025-11-06 12:12:19 -0500

    11/6/2025

--- src/com/mac286/heaps/OurHeap.java
+++ src/com/mac286/heaps/OurHeap.java
@@ -1,5 +1,7 @@
 package com.mac286.heaps;
 
+import java.util.Arrays;
+
 public class OurHeap <T extends Comparable<T>>{
     //We need an array of Ts which are Comparables. Create an array of Comparables
     private T[] H;
@@ -18,24 +20,61 @@ public class OurHeap <T extends Comparable<T>>{
 
     public void add(T e) {
         //if full, then resize
-
+        if(size == H.length){
+            H = Arrays.copyOf(H, H.length*2);
+        }
         //add e at index size;
-
+        H[size] = e;
         //heapify at index size
-
+        heapify(size);
         //increment size
+        size++;
     }
 
     private void heapify(int i){
         //if i is the root return
+        if (i <= 0)
+            return;
         //find the parent of i
-
+        int parent = (i-1)/2;
         //compare i to the parent if i is larger, then return
-
+        if(H[i].compareTo(H[parent]) >= 0){
+            //i is larger than the parent, return
+            return;
+        }
         //swap i with parent
-
+        T save = H[i];
+        H[i] = H[parent];
+        H[parent] = save;
         //heapify at the parent
+        heapify(parent);
 
+    }
+//TODO: HW7
+    public T remove(){
+        //save the top, index 0
+
+        //replace the top 0 by the last (index size-1)
+
+        //decrement size.
+
+        //reheapify(0)
+
+        //retrun save
+    }
+//TODO: HW7
+    private void reheapify(int i){
+        //if i has no children, return.
+        //left child = 2*i + 1, right cxhild = 2*i + 2
+        //if no left child, then no children.
+
+        //find the smallest of the children, call it smallest
+
+        //compare i to smallest, if i is smaller, then return
+
+        //swap i with smallest
+
+        //reheapify at smallest
     }
     //Another way of dealing with the comma problem
     public String toString(){
--- src/com/mac286/heaps/PriorityQeueueTester.java
+++ src/com/mac286/heaps/PriorityQeueueTester.java
@@ -4,18 +4,21 @@ import java.util.PriorityQueue;
 
 public class PriorityQeueueTester {
     public static void main(String[] args) {
-        PriorityQueue<Integer> Q = new PriorityQueue<>();
+        //PriorityQueue<Integer> Q = new PriorityQueue<>();
+        OurHeap<Integer> Q = new OurHeap<>();
         Q.add(-2);
         Q.add(-6);
         Q.add(-5);
+        System.out.println("Q: " + Q);
         Q.add(-13);
         Q.add(-9);
         Q.add(-8);
         System.out.println("Q: " + Q);
-        System.out.println("Removing: " + Q.remove());
+        /*System.out.println("Removing: " + Q.remove());
         System.out.println("Q: " + Q);
         System.out.println("Removing: " + Q.remove());
         System.out.println("Q: " + Q);
+         */
 
     }
 }
